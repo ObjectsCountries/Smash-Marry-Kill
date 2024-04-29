b0f79ede92427bb3e20ccff90cd692b8913e4935
ObjectsCountries <oandcofficial@gmail.com>
2024-04-29 07:51:56 -0700

    amogus gyatt

---
+++ Assets/_smkTP.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using Wawa.TwitchPlays;
+using Wawa.TwitchPlays.Domains;
+
+public sealed class _smkTP: Twitch<_smashmarrykill>{
+    public override IEnumerable<Instruction>ForceSolve(){
+        yield return null; 
+    }
+}
