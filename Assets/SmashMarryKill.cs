ed80ad23d81abb5db9fa33d6cac1aeb092a13956
ObjectsCountries <oandcofficial@gmail.com>
2024-04-29 20:46:38 -0700

    Update wawa, begin development

--- Assets/SmashMarryKill.cs
+++ Assets/SmashMarryKill.cs
@@ -1,4 +1,5 @@
-﻿using Wawa.Modules;
+﻿using Wawa.Extensions;
+using Wawa.Modules;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,15 +12,48 @@ public class SmashMarryKill : ModdedModule
 
     public KMSelectable module;
     public TextMesh result;
+    public KMSelectable[] candidates;
+    public TextMesh[] candidateNames;
+
+    private int currentIndex = 0;
+    ///<value>The words "SMASH", "MARRY" and "KILL", for ease of use.</value>
+    private enum SMKwords
+    {
+        SMASH,
+        MARRY,
+        KILL
+    }
 
     protected override void Awake()
     {
-        // amogus gyatt 
+        foreach (KMSelectable candidate in candidates)
+        {
+            KMSelectable unused = candidate.Set(onInteract: () =>
+            {
+                if (candidate.GetComponentInChildren<TextMesh>().color == Color.white)
+                {
+                    SMKselect(currentIndex, candidate, candidate.GetComponentInChildren<TextMesh>());
+                    currentIndex += 1;
+                    currentIndex %= 3;
+                    if (currentIndex == 0)
+                    {
+                        foreach (KMSelectable candidate_ in candidates)
+                        {
+                            candidate_.GetComponentInChildren<TextMesh>().color = Color.white;
+                        }
+                    }
+                }
+            });
+        }
     }
 
-    private void Start()
+    ///<summary>Selection of Smash/Marry/Kill.</summary>
+    ///<param name="index">The current index of the Smash/Marry/Kill cycle.</param>
+    ///<param name="candidateButton">The button to alter.</param>
+    ///<param name="candidateName">The button's TextMesh to alter.</param>
+    private void SMKselect(int index, KMSelectable candidateButton, TextMesh candidateName)
     {
-
+        result.text = "" + (SMKwords)index;
+        candidateName.color = Color.green;
     }
-
 }
--- Assets/smkTP.cs
+++ Assets/smkTP.cs
@@ -4,6 +4,7 @@ using Wawa.TwitchPlays.Domains;
 
 public sealed class smkTP : Twitch<SmashMarryKill>
 {
+    [Command("")]
     public override IEnumerable<Instruction> ForceSolve()
     {
         yield return null;
