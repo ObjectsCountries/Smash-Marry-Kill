b311971ffd73116031fe3478f8f1cde1e90c4617
ObjectsCountries <oandcofficial@gmail.com>
2024-04-29 17:20:40 -0700

    Lint scripts

--- Assets/_smkTP.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using Wawa.TwitchPlays;
-using Wawa.TwitchPlays.Domains;
-
-public sealed class _smkTP: Twitch<_smashmarrykill>{
-    public override IEnumerable<Instruction>ForceSolve(){
-        yield return null; 
-    }
-}
